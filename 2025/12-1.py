88d28e259cf689a8ab55b084e27b6ac98092ae30
César Pedraza <cpedraza@unal.edu.co>
2026-03-13 19:07:19 -0500

    add day 12 part 1 (that was painful you know?)

--- 2025/12-1.py
+++ 2025/12-1.py
@@ -1,50 +1,8 @@
 def canTheyFit(grid, presents):
-    if len(presents) == 0:
-        return True
-    print(presents)
-    for i in range(len(presents)):
-        present = presents[i]
-        for position in whereToPut(present, grid):
-            presents.pop(i)
-            for version in versions(present):
-                insert(version, position, grid)
-                if canTheyFit(grid, presents):
-                    return True
-                remove(version, position, grid)
-            presents.insert(i, present)
-    
-    return False
+    gridArea = len(grid) * len(grid[0])
+    presentsMinArea = sum(sum(sum(line) for line in present) for present in presents)
 
-def whereToPut(present, grid):
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            if itFits((y,x), present, grid):
-                yield (y,x)
-
-def itFits(position, present, grid):
-    y0, x0 = position
-    for y in range(len(present)):
-        for x in range(len(present[y])):
-            try:
-                if not (present[y][x] and not grid[y + y0][x + x0]):
-                    return False
-            except: continue
-
-    return True
-
-def insert(present, position, grid):
-    y0, x0 = position
-    for y in range(len(present)):
-        for x in range(len(present[y])):
-            if present[y][x]:
-                grid[y + y0][x + x0] = 1
-
-def remove(present, position, grid):
-    y0, x0 = position
-    for y in range(len(present)):
-        for x in range(len(present[y])):
-            if present[y][x]:
-                grid[y + y0][x + x0] = 0
+    return presentsMinArea <= gridArea
 
 def parsePresent(presentText):
     return [
@@ -62,7 +20,7 @@ def parseProblems(problemsText, presents):
         for index, amount in enumerate(presentAmountList):
             presentList += [presents[index]]*amount
         
-        yield ([[0]*width]*length, presentList)
+        yield ([[0 for _ in range(width)] for _ in range(length)], presentList)
 
 def parse(inputText):
     items = inputText.split('\n\n')
